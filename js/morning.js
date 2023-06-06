01d71607973f377e1a62d04e217737692af78db2
salathiel wekesa <107699372+salawekesa@users.noreply.github.com>
2023-06-06 23:32:53 +0300

    changes

--- js/morning.js
+++ js/morning.js
@@ -8,4 +8,9 @@ function countWords() {
     
     document.getElementById('wordCount').textContent = 'Word Count: ' + wordCount;
     
+    if (wordCount >= 500) {
+        document.getElementById('balloonContainer').style.display = 'block';
+    } else {
+        document.getElementById('balloonContainer').style.display = 'none';
+    }
 }
